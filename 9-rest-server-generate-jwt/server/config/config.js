1a453aace0bfaa4d50f032673e007297fbf98f50
Jesús Garrido <jesus.garrido@technest.es>
2021-02-03 18:26:24 +0100

    Rest server example 9 generate jwt

---
+++ 9-rest-server-generate-jwt/server/config/config.js
@@ -0,0 +1,14 @@
+// ============================
+//  PORT
+// ============================
+process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  TOKEN EXPIRY
+// ============================
+process.env.TOKEN_EXPIRY = "2h";
+
+// ============================
+//  TOKEN SECRET
+// ============================
+process.env.SEED = process.env.SEED || "this is a secret code";
